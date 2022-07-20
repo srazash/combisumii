23b3d238d86b4259baa21cd1b56a2495d4f6916b
Ryan Shaw-Harrison <srazash@protonmail.com>
2022-07-20 08:37:14 +0100

    Updated main.zig, testing rng fn.

--- src/main.zig
+++ src/main.zig
@@ -2,10 +2,11 @@ const std = @import("std");
 
 pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
-    try stdout.print("RNG: {d}\n", .{rng(1,10)});
+    try stdout.print("RNG: {d}\n", .{rng(0,1)});
 }
 
 fn rng(lower: u8, upper: u8) !u8 {
+    // note: the lower and upper values are inclusive
     var prng = std.rand.DefaultPrng.init(blk: {
         var seed: u64 = undefined;
         try std.os.getrandom(std.mem.asBytes(&seed));
