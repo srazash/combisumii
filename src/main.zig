168098fcb332261ac2205f5d6e01e7a13f63fc6a
Ryan Shaw-Harrison <srazash@protonmail.com>
2022-07-20 08:35:19 +0100

    Updated rng fn to accept a lower/upper limit.

--- src/main.zig
+++ src/main.zig
@@ -2,10 +2,10 @@ const std = @import("std");
 
 pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
-    try stdout.print("RNG: {d}\n", .{rng()});
+    try stdout.print("RNG: {d}\n", .{rng(1,10)});
 }
 
-fn rng() !i32 {
+fn rng(lower: u8, upper: u8) !u8 {
     var prng = std.rand.DefaultPrng.init(blk: {
         var seed: u64 = undefined;
         try std.os.getrandom(std.mem.asBytes(&seed));
@@ -13,5 +13,5 @@ fn rng() !i32 {
     });
     const rand = prng.random();
     
-    try return rand.int(u8);
+    try return rand.intRangeAtMost(u8, lower, upper);
 }
