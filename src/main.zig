30b9468e4586d96f8e277cb1ff7fd2d3b0ef5996
Ryan Shaw-Harrison <srazash@protonmail.com>
2022-08-03 18:24:46 +0100

    Investigating reading user input.

--- src/main.zig
+++ src/main.zig
@@ -2,21 +2,26 @@ const std = @import("std");
 
 pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
+    const stdin = std.io.Reader;
 
     const array = std.ArrayList;
     const alloc = std.heap.page_allocator;
-    
+
     var candidates = array(u8).init(alloc);
-    
+
+    try stdout.print("What is the target? :", .{});
+    var target: u8 = try stdin.read();
+    try stdout.print("\n\n", .{}); // generates error --- why?
+
     var arraylen: u8 = 1;
-    while (arraylen <= try rng(1,100)) : (arraylen += 1) {
-        try candidates.append(try rng(1,50));
+    while (arraylen <= try rng(1, 100)) : (arraylen += 1) {
+        try candidates.append(try rng(1, 50));
     }
 
     try stdout.print("# of candidates: {d}\n", .{candidates.items.len});
     try stdout.print("Candidates: {d}\n", .{candidates.items});
 
-    const target: u8 = 30; // chnaged target to const
+    //const target: u8 = 30; // chnaged target to const
     //var target: u8 = try rng(1,30);
 
     try stdout.print("Target: {d}\n", .{target});
@@ -37,6 +42,6 @@ fn rng(lower: u8, upper: u8) !u8 {
         break :blk seed;
     });
     const rand = prng.random();
-    
+
     try return rand.intRangeAtMost(u8, lower, upper);
 }
